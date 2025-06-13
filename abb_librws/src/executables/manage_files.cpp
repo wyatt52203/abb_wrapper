9b58098b50783178bdcd7aaf5dd6f41b2bfece94
wyatt52203 <wyatt@tyson.build>
2025-06-13 13:18:47 +0000

    made file for getting and printing existing files on controller

---
+++ abb_librws/src/executables/manage_files.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <abb_librws/rws_interface.h>
+#include <Poco/Net/Context.h>
+
+int main()
+{
+    std::string ip = "192.168.15.81";
+    std::string username = "Admin";
+    std::string password = "robotics";
+
+    // Create Poco SSL context with no verification (self-signed certs likely)
+    const Poco::Net::Context::Ptr ptrContext(new Poco::Net::Context( Poco::Net::Context::CLIENT_USE, "", "", "", Poco::Net::Context::VERIFY_NONE));
+
+    // Create RWS interface
+    abb::rws::RWSInterface rws_interface(ip, username, password, ptrContext);
+
+    // Turn off existing processes
+    std::cout << "program off: " << rws_interface.stopRAPIDExecution() << std::endl;
+
+    std::string file_content;
+    abb::rws::RWSClient::FileResource resource("", "/Home/Programs/Wizard");
+    std::cout << "get file: " << rws_interface.getFile(resource, &file_content) << std::endl;
+
+    std::cout << "File content:\n" << file_content << std::endl;
+
+    return 0;
+}
